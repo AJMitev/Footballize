76cafcb93c37e76c014db5fa11d5f50f7d41c589
AJMitev <mail@aleksandar.it>
2019-07-28 15:34:17 +0300

    Implement ProvinceService

---
+++ src/Footballize/Footballize.Services/Data/IProvinceServices.cs
@@ -0,0 +1,12 @@
+﻿namespace Footballize.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+
+    public interface IProvinceServices
+    {
+        IEnumerable<TViewModel> GetProvinces<TViewModel>();
+        Task CreateProvince(Province province);
+    }
+}
---
+++ src/Footballize/Footballize.Services/Data/ProvinceService.cs
@@ -0,0 +1,33 @@
+﻿namespace Footballize.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Footballize.Data.Repositories;
+    using Mapping;
+    using Models;
+
+    public class ProvinceService : IProvinceServices
+    {
+        private readonly IRepository<Province> provincesRepository;
+
+        public ProvinceService(IRepository<Province> provincesRepository)
+        {
+            this.provincesRepository = provincesRepository;
+        }
+
+        public IEnumerable<TViewModel> GetProvinces<TViewModel>()
+        {
+            return this.provincesRepository
+                .All()
+                .OrderBy(x=>x.Name)
+                .To<TViewModel>();
+        }
+
+        public async Task CreateProvince(Province province)
+        {
+            await this.provincesRepository.AddAsync(province);
+            await this.provincesRepository.SaveChangesAsync();
+        }
+    }
+}
