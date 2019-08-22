39232c4cf454674905549eed7beca7b0b99e0924
Aleksander Mitev <mail@aleksandar.it>
2019-08-22 19:13:17 +0300

    Add username to registering form.

--- src/Footballize/Footballize.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Footballize/Footballize.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -58,6 +58,11 @@ namespace Footballize.Web.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [StringLength(16,ErrorMessage = StringLengthErrorMessage, MinimumLength = 4)]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = StringLengthErrorMessage, MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -79,7 +84,7 @@ namespace Footballize.Web.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName, CreatedOn = DateTime.UtcNow};
+                var user = new User { UserName = Input.Username, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName, CreatedOn = DateTime.UtcNow};
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
