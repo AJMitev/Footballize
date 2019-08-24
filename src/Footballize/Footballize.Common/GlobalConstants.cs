974368e818f3aaa81f2b090ccda6d147da1ed689
Aleksander Mitev <mail@aleksandar.it>
2019-08-24 18:35:07 +0300

    Update password hashing

---
+++ src/Footballize/Footballize.Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Footballize.Common
+{
+    public class GlobalConstants
+    {
+        public const string CanCreateGathersRoleName = "CanCreateGathers";
+        public const string CanEditGathersRoleName = "CanEditGathers";
+        public const string CanDeleteGathersRoleName = "CanDeleteGathers";
+        public const string CanCreateRecruitmentRoleName = "CanCreateRecruitment";
+        public const string CanEditRecruitmentRoleName = "CanEditRecruitment";
+        public const string CanDeleteRecruitmentRoleName = "CanDeleteRecruitment";
+        public const string CanSeeAdminAreaRoleName = "CanSeeAdminArea";
+    }
+}
