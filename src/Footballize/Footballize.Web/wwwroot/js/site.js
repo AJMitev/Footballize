6ffa972822e692232fe7bf9d0b7db1e31da7aa73
AJMitev <mail@aleksandar.it>
2019-08-30 23:26:50 +0300

    Update static files

--- src/Footballize/Footballize.Web/wwwroot/js/site.js
+++ src/Footballize/Footballize.Web/wwwroot/js/site.js
@@ -3,7 +3,6 @@
 
 // Write your JavaScript code.
 function initMap(latitude, longitude) {
-    debugger;
     var location =
     {
 
@@ -23,4 +22,87 @@ function initMap(latitude, longitude) {
             position: location,
             map: map
         });
+}
+
+
+function initDatetimepicker() {
+    $('.datetimepicker').datetimepicker({
+        minDate: new Date(),
+            
+        format: 'DD-MM-YYYY HH:mm',
+        showTodayButton: true,
+        sideBySide : true,
+           
+        showClose: true,
+        showClear: true,
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-chevron-up",
+            down: "fa fa-chevron-down",
+            previous: 'fa fa-chevron-left',
+            next: 'fa fa-chevron-right',
+            today: 'fa fa-screenshot',
+            clear: 'fa fa-trash',
+            close: 'fa fa-remove'
+        }
+    });
+}
+
+
+function startChat(gatherId) {
+    var entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '/': '&#x2F;',
+        '`': '&#x60;',
+        '=': '&#x3D;'
+    };
+
+    function escapeHtml (string) {
+        return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+            return entityMap[s];
+        });
+    }
+
+    var connection =
+        new signalR.HubConnectionBuilder()
+            .withUrl("/chat")
+            .build();
+
+    connection.on("NewMessage",
+        function (message) {
+            var chatInfo = `<div><strong>${escapeHtml(message.user)}</strong>: ${escapeHtml(message.text)}</div>`;
+            $("#messagesList").append(chatInfo);
+        });
+
+
+    connection.on("LoadChatHistory", function() {
+        connection.invoke("GetHistory", gatherId);
+    });
+
+    connection.on("UserJoin",
+        function (message) {
+            var chatInfo = `<div class="font-italic">${escapeHtml(message.user)} is connected to chat room.</div>`;
+            $("#messagesList").append(chatInfo);
+        });
+
+    connection.on("UserLeave",
+        function (message) {
+            var chatInfo = `<div class="font-italic">${escapeHtml(message.user)} has left the chat room.</div>`;
+            $("#messagesList").append(chatInfo);
+        });
+
+    $("#sendButton").click(function() {
+        var message = $("#messageInput").val();
+        connection.invoke("Send", message, gatherId);
+        $("#messageInput").val("");
+    });
+
+    connection.start().catch(function (err) {
+        return console.error(err.toString());
+    });
 }
