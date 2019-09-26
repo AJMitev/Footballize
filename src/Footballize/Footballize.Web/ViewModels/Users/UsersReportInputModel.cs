b3eddf1a4d2c42e498ac832a1977f4333f478911
AJMitev <mail@aleksandar.it>
2019-09-26 18:45:22 +0300

    All variables in view models are now constants.

--- src/Footballize/Footballize.Web/ViewModels/Users/UsersReportInputModel.cs
+++ src/Footballize/Footballize.Web/ViewModels/Users/UsersReportInputModel.cs
@@ -8,9 +8,14 @@
 
     public class UsersReportInputModel : IMapTo<UserReport>, IMapFrom<User>, IHaveCustomMappings
     {
+        private const int ReportTypeRangeStart = 1;
+        private const int ReportTypeRangeEnd = 2;
+        private const int TextMinimumLength = 5;
+        private const int TextMaximumLength = 350;
+
         [Required]
         [Display(Name = "Username")]
-        public string UserName{ get; set; }
+        public string UserName { get; set; }
 
         [Required]
         [Display(Name = "First Name")]
@@ -24,12 +29,12 @@
         public string Id { get; set; }
 
         [Required]
-        [Range(1,2)]
+        [Range(ReportTypeRangeStart, ReportTypeRangeEnd)]
         public ReportType Type { get; set; }
 
         [Required]
         [DataType(DataType.MultilineText)]
-        [StringLength(350,MinimumLength = 5)]
+        [StringLength(TextMaximumLength, MinimumLength = TextMinimumLength)]
         public string Text { get; set; }
 
         public void CreateMappings(IMapperConfigurationExpression configuration)
