d4e2b306084c93eea1e4b66f4c13346a2dab9c76
AJMitev <mail@aleksandar.it>
2019-10-18 10:46:44 +0300

    Separate Mapings from services

--- src/Footballize/Footballize.Services.Mapping/AutoMapperConfig.cs
+++ src/Footballize/Footballize.Services.Mapping/AutoMapperConfig.cs
@@ -48,6 +48,7 @@
                         map.CreateMappings(configuration);
                     }
                 });
+
             MapperInstance = new Mapper(new MapperConfiguration(config));
         }
 
@@ -85,14 +86,15 @@
             return toMaps;
         }
 
+
         private static IEnumerable<IHaveCustomMappings> GetCustomMappings(IEnumerable<Type> types)
         {
             var customMaps = from t in types
-                from i in t.GetTypeInfo().GetInterfaces()
-                where typeof(IHaveCustomMappings).GetTypeInfo().IsAssignableFrom(t) &&
-                      !t.GetTypeInfo().IsAbstract &&
-                      !t.GetTypeInfo().IsInterface
-                select (IHaveCustomMappings) Activator.CreateInstance(t);
+                             from i in t.GetTypeInfo().GetInterfaces()
+                             where typeof(IHaveCustomMappings).GetTypeInfo().IsAssignableFrom(t) &&
+                                   !t.GetTypeInfo().IsAbstract &&
+                                   !t.GetTypeInfo().IsInterface
+                             select (IHaveCustomMappings)Activator.CreateInstance(t);
 
             return customMaps;
         }
